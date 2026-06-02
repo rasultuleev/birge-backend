e9eeeb3323d7fd29a6bf75084090155bc65c0a0a
rasultuleev <75238687+rasultuleev@users.noreply.github.com>
2026-06-02 08:48:23 +0500

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn birge.wsgi:application --bind 0.0.0.0:$PORT
---
+++ gunicorn.conf.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birge.settings')
