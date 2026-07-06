500355c5eb3204d71c6f91e9a094be8e171a754a
rasultuleev <75238687+rasultuleev@users.noreply.github.com>
2026-07-06 14:12:06 +0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('health/', views.health_check),
     path('send-code/', views.send_sms_code),
     path('verify-code/', views.verify_code),
+    path('profile/', views.get_profile),
     path('profile/update/', views.update_profile),
     path('register-event/', views.register_event),
+    path('events/', views.event_list),
 ]
