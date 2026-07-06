b2a85f92fe8f9c98145f2bcd532b496c9ffab9b6
rasultuleev <75238687+rasultuleev@users.noreply.github.com>
2026-07-06 14:15:39 +0500

    Update settings.py

--- birge/settings.py
+++ birge/settings.py
@@ -135,3 +135,14 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
