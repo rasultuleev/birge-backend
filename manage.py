ea74526cf6ae0a8302609b68946ddb736088a6f9
rasultuleev <75238687+rasultuleev@users.noreply.github.com>
2026-07-07 11:49:47 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management import call_command
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'birge.settings')
+    
+    # Автоматически применяем миграции при запуске
+    call_command('migrate', interactive=False)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
